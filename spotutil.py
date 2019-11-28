5e3dfaabe6292e5190c03180ee87d339f3b16aa0
Alan Layng <alan@readersludlow.com>
2019-11-28 17:21:12 +0000

    upgraded to v2

--- spotutil.py
+++ spotutil.py
@@ -10,13 +10,19 @@ Player
 
 """
 
-import yaml, logging, threading, collections, queue, json
+import logging
+import threading
+import collections
+import queue
+import json
 from pathlib import Path
-from bunch import *
+
+BYTES_PER_SECOND = 2 * 2 * 44100
+BLANK_500MS = b'\0' * (BYTES_PER_SECOND // 2)
+TIMEOUT = 120
 
 session = None # global session
 logger = logging.getLogger(__name__)
-Image = None
 
 class _Error(Exception):
     def __init__(self, message):
@@ -29,32 +35,26 @@ class LoginError(_Error): pass
 class PlayError(_Error): pass
 
 def start(credentials=None,
-            disable_spotify_logging=True):
+          disable_spotify_logging=True):
 
     if credentials is None:
         with Path(__file__).with_name('credentials.json').open() as f:
-            credentials = bunchify(json.load(f))
+            credentials = json.load(f)
     c = credentials
     if 'appkey64' in c and 'appkey' not in c:
         import base64
-        c.appkey = base64.b64decode(c.appkey64)
+        c['appkey'] = base64.b64decode(c['appkey64'])
 
     logger.debug('loading pyspotify')
     import spotify
     if disable_spotify_logging:
         logging.getLogger('spotify').setLevel(logging.WARN)
-        #sl = logging.getLogger('spotify')
-        #for h in sl.handlers:
-        #    sl.removeHandler(h)
-        #sl.addHandler(logging.NullHandler())
-        #sl.propogate=False
-        #sl.setLevel(logging.ERROR)
     logger.debug("pyspotify loaded")
 
     cfg = spotify.Config()
-    cfg.application_key = c.appkey
-    cfg.cache_location = c.cachedir
-    cfg.settings_location = c.cachedir
+    cfg.application_key = c['appkey']
+    cfg.cache_location = c['cachedir']
+    cfg.settings_location = c['cachedir']
     global session
     session = spotify.Session(cfg)
     session.preferred_bitrate(spotify.Bitrate.BITRATE_320k)
@@ -67,13 +67,26 @@ def start(credentials=None,
     loop = spotify.EventLoop(session)
     loop.start()
     session.on(spotify.SessionEvent.CONNECTION_STATE_UPDATED,
-            connection_state_changed)
+               connection_state_changed)
 
-    session.login(c.username, c.password)
+    session.login(c['username'], c['password'])
     if not logged_in.wait(60):
         raise LoginError('Failed to login after 60 seconds')
     logger.debug("logged in to spotify")
 
+class Packet: pass
+
+class EndPacket(Packet): pass
+
+class ErrorPacket(Packet):
+    def __init__(self, error):
+        self.error = error
+
+class MusicPacket(Packet):
+    def __init__(self, audio_format, frames, num_frames):
+        self.audio_format = audio_format
+        self.frames = frames
+        self.num_frames = num_frames
 
 class Player:
     """
@@ -92,23 +105,12 @@ class Player:
     last 22050 samples if they are all zero
 
     """
-    MUSIC = 'music'
-    END_OF_TRACK = 'end_of_track'
-    ERROR = 'error'
-    BLANK_500MS = b'\0' * (2 * 2 * 44100 // 2) # 2 channels @ 2 bytes
-    TIMEOUT=30
 
     def __init__(self, uri, maxqsize=10):
         self._playing = False
         self.track = session.get_track(uri).load() # track to play
         self._postbox = queue.Queue()
         self._maxqsize = maxqsize # dont let queue grow larger than this
-        self._notify = None
-        self._notify_period = self._next_notify = 0
-
-    def set_notify(self, callback, freq=60*44100):
-        self._notify = callback
-        self._notify_period = freq
 
     def _start(self):
         self._set_callbacks()
@@ -123,6 +125,35 @@ class Player:
         self._clear_callbacks()
         self._playing = False
 
+    def _set_callbacks(self):
+        from spotify import SessionEvent as se
+        session.on(se.CONNECTION_ERROR, self.on_error)
+        session.on(se.STREAMING_ERROR, self.on_error)
+        session.on(se.MUSIC_DELIVERY, self.on_music)
+        session.on(se.PLAY_TOKEN_LOST, self.on_play_token_lost)
+        session.on(se.END_OF_TRACK, self.on_end_of_track)
+
+    def _clear_callbacks(self):
+        from spotify import SessionEvent as se
+        session.off(se.CONNECTION_ERROR, self.on_error)
+        session.off(se.STREAMING_ERROR, self.on_error)
+        session.off(se.MUSIC_DELIVERY, self.on_music)
+        session.off(se.PLAY_TOKEN_LOST, self.on_play_token_lost)
+        session.off(se.END_OF_TRACK, self.on_end_of_track)
+
+    def on_end_of_track(self, session):
+        "Callback when spotify has finished sending data"
+        self.stop()
+        self._postbox.put(EndPacket())
+
+    def on_error(self, session, error_type):
+        self.stop()
+        self._postbox.put(ErrorPacket(repr(error_type)))
+
+    def on_play_token_lost(self, session):
+        self.stop()
+        self._postbox.put(ErrorPacket('Play token lost'))
+
     def on_music(self, session, audio_format, frames, num_frames):
         """Receives the music from spotify
 
@@ -131,70 +162,10 @@ class Player:
         if self._postbox.qsize() > self._maxqsize:
             return 0 # nothing consumed, try again later
 
-        self._postbox.put(Bunch(
-            cmd=self.MUSIC,
-            frames=frames,
-            num_frames=num_frames,
-            sample_rate=audio_format.sample_rate,
-            channels=audio_format.channels))
+        self._postbox.put(MusicPacket(audio_format, frames, num_frames))
         return num_frames # consumed them all
 
-    def on_end_of_track(self, session):
-        "Callback when spotify has finished sending data"
-        self.stop()
-        self._postbox.put(Bunch(
-            cmd=self.END_OF_TRACK))
-
-    def on_error(self, session, error_type):
-        self.stop()
-        self._postbox.put(Bunch(
-            cmd=self.ERROR,
-            error=repr(error_type)))
-
-    def on_play_token_lost(self, session):
-        self.stop()
-        self._postbox.put(Bunch(
-            cmd=self.ERROR,
-            error='Play token lost'))
-
-    def _set_callbacks(self):
-        from spotify import SessionEvent as se
-        session.on(se.CONNECTION_ERROR, self.on_error)
-        session.on(se.STREAMING_ERROR,  self.on_error)
-        session.on(se.MUSIC_DELIVERY,   self.on_music)
-        session.on(se.PLAY_TOKEN_LOST,  self.on_play_token_lost)
-        session.on(se.END_OF_TRACK,     self.on_end_of_track)
-
-    def _clear_callbacks(self):
-        from spotify import SessionEvent as se
-        session.off(se.CONNECTION_ERROR, self.on_error)
-        session.off(se.STREAMING_ERROR,  self.on_error)
-        session.off(se.MUSIC_DELIVERY,   self.on_music)
-        session.off(se.PLAY_TOKEN_LOST,  self.on_play_token_lost)
-        session.off(se.END_OF_TRACK,     self.on_end_of_track)
-
-    def _handle_packet(self, packet):
-        """Processes a packet of data, performing notifies if 
-        needed, and returning the tuple to be yielded to the client
-        """
-        self._frame_count += packet.num_frames
-        ex = Bunch(
-            frame_count=self._frame_count,
-            num_frames=packet.num_frames,
-            sample_rate=packet.sample_rate,
-            channels=packet.channels)
-
-        # call a notify (if set) if due
-        while self._notify and self._frame_count >= self._next_notify:
-            logger.debug(
-                    "Calling notify after %d frames",
-                    self._next_notify)
-            self._notify(self._next_notify)
-            self._next_notify += self._notify_period
-
-        return (packet.frames, ex)
-
-    def get_data_ex(self):
+    def get_data(self, end_after=None):
         """
         The meat of the capture
 
@@ -206,63 +177,58 @@ class Player:
         """
         logger.debug('Starting playback')
         self._start()
-        self._frame_count = 0
-        self._next_notify = self._notify_period
+        byte_count = 0
         cache = collections.deque()
         min_cache_size = 1 # to contain the last packet
                            # but could be set higher if we want
 
         while True:
             try:
-                packet = self._postbox.get(timeout=self.TIMEOUT)
+                packet = self._postbox.get(timeout=TIMEOUT)
             except queue.Empty:
                 raise PlayError('Timed out waiting for data')
-            if packet.cmd == self.ERROR:
+            if isinstance(packet, ErrorPacket):
                 raise PlayError(packet.error)
-            if packet.cmd == self.END_OF_TRACK:
-                logger.debug('End of track received')
-                break # Nothing left, so succesful end
-            assert packet.cmd == self.MUSIC
+            if isinstance(packet, EndPacket):
+                break
+            assert isinstance(packet, MusicPacket)
+            byte_count += len(packet.frames)
+            if end_after and byte_count > end_after * BYTES_PER_SECOND:
+                logger.debug('Stopping early as requested')
+                self.stop()
+                return
             cache.append(packet)
-            while len(cache)>min_cache_size:
-                yield self._handle_packet(cache.popleft())
+            while len(cache) > min_cache_size:
+                packet = cache.popleft()
+                yield packet.frames
 
         # we have finished receiving, so we drain the cache, removing
         # any padded silence
         while len(cache):
             packet = cache.popleft()
-            if packet.frames == self.BLANK_500MS:
+            if packet.frames == BLANK_500MS:
                 logger.info('Skipping final 500ms of silence')
                 pass # do not yield it
             else:
-                yield self._handle_packet(packet)
+                yield packet.frames
 
         # and that's it - all finished
-
-
-    def get_data(self):
-        for data, ex in self.get_data_ex():
-            yield data
+        logger.debug('All packets processed')
 
 
 def test():
     logging.basicConfig(level=logging.DEBUG)
 
     start()
-    t1,t2=session.get_album('spotify:album:4qWzxEpG4gax8CGVQUFwqA')\
+    t1, t2 = session.get_album('spotify:album:4qWzxEpG4gax8CGVQUFwqA')\
             .browse().load().tracks[22:24]
-    def notify(n):
-        m,s = divmod(n//44100,60)
-        logger.info("Got %02d:%02d of music",m,s)
-    p=Player(str(t1.link))
-    p.set_notify(notify,5*44100)
+    p = Player(str(t1.link))
     with Path('test.pcm').open('wb') as f:
-        for d in p.get_data():
+        for d in p.get_data(10):
             f.write(d)
-    p=Player(str(t2.link))
-    p.set_notify(notify,2*44100)
+    p = Player(str(t2.link))
     with Path('test2.pcm').open('wb') as f:
-        for d in p.get_data():
+        for d in p.get_data(10):
             f.write(d)
 
 if __name__ == '__main__':
--- spotweb.py
+++ spotweb.py
@@ -1,13 +1,12 @@
 #
 # std library
 #
-import logging, subprocess, threading, collections, time, signal
+import logging, threading, signal
 
 #
 # 3rd party
 #
 import bottle, clize
-from bunch import *
 
 #
 # local
@@ -16,47 +15,41 @@ import spotutil
 
 logger = logging.getLogger(__name__)
 
-VERSION = "1.0.1"
+VERSION = "2.0.1"
 
 #
 # Globals
 #
 
+BYTES_PER_SECOND = 2 * 2 * 44100
+
+class Status:
+    def __init__(self):
+        self.streaming = self.streamed = False
+        self.uri = None
+        self.error = None
+        self.bytes = 0
+    def reset(self, uri):
+        self.streaming = self.streamed = False
+        self.uri = uri
+        self.error = None
+        self.bytes = 0
+    def to_dict(self):
+        return self.__dict__.copy()
+
+
 app = bottle.Bottle()                # the global app
-status = Bunch(streaming=False)  # current server status
-receipts = collections.OrderedDict()
-receiptPeriod = 12 * 3600   # keep receipts for how long (12 hours)
+status = Status()
 
-
-
-def main(*, port=39704, debug=False, timeout=300):
-
+def main(*, port=39705, server='waitress', debug=False):
     if debug:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.WARN)
 
     spotutil.start()
-
-    # from https://gist.github.com/coffeesnake/3093598
-    from wsgiref.simple_server import make_server, WSGIServer
-    from socketserver import ThreadingMixIn
-    class ThreadingWSGIServer(ThreadingMixIn, WSGIServer): pass
-
-    httpd = make_server('0.0.0.0', port, app, ThreadingWSGIServer)
-
-    def shutdown(signum, frame):
-        # run shutdown from another thread
-        threading.Thread(target=httpd.shutdown).start()
-        print("Server shut down")
-
-    signal.signal(signal.SIGTERM, shutdown)
-    #threading.Thread(target=idle_thread, args=(httpd, timeout)).start()
     print("Spotweb v%s" % VERSION)
-    print('Listening on port %d....' % port)
-    httpd.serve_forever()
-
-
+    bottle.run(app, server=server, port=port)
 
 #
 # Main Routing Entry Points
@@ -65,238 +58,82 @@ def main(*, port=39704, debug=False, timeout=300):
 @app.route('/album/<album_id>')
 def album_details(album_id):
     uri = _expand_uri('spotify:album:', album_id)
-    logger.debug('Getting %s', uri)
-    a = get_album_details(uri)
-    return unbunchify(a)
+    return format_album(spotutil.session.get_album(uri))
 
-@app.route('/cover/<album_id>')
-def album_cover(album_id):
-    uri = _expand_uri('spotify:album:', album_id)
-    logger.debug('Getting cover for %s', uri)
-    bottle.response.content_type = 'image/jpeg'
-    return get_album_cover(uri)
+@app.route('/track/<track_id>')
+def track_details(track_id):
+    uri = _expand_uri('spotify:track:', track_id)
+    return format_track(spotutil.session.get_track(uri))
 
 @app.route('/play/<track_id>')
 def play_track(track_id):
     uri = _expand_uri('spotify:track:', track_id)
     if status.streaming: bottle.abort(403)
-    request = bottle.request
-    response = bottle.response
-    fmt = request.query.format or 'flac'
-    rcpt = make_receipt(uri)
-    if fmt == 'flac':
-        response.content_type = 'audio/flac'
-        rcpt.format = 'flac'
-    elif fmt == 'raw':
-        response.content_type = 'audio/x-pcm'
-        rcpt.format = 'raw'
-    else:
-        bottle.abort(415)
-    data = get_track_pcm(uri, rcpt)
-    if fmt == 'flac':
-        data = flac_encode(data, rcpt)
-    yield from data
-    logger.debug("Final receipt=%r", rcpt)
-
-@app.route('/receipt/<track_id>')
-def send_receipt(track_id):
-    uri = _expand_uri('spotify:track:', track_id)
-    if not uri in receipts: bottle.abort(404)
-    return unbunchify(receipts[uri])
-
-@app.route('/status')
-def send_status():
-    s = Bunch(status=status, receipts=receipts)
-    return unbunchify(s)
-
-#
-# Playing routines
-#
-
-def get_track_pcm(uri, rcpt):
-    logger.debug('streaming started')
     player = spotutil.Player(uri)
-    # throw 403 if track not playable
     if player.track.availability != 1: bottle.abort(403)
-    status.streaming = True
-    status.uri = uri
-    status.length = player.track.duration // 1000
-    status.pos = 0
-    def notify(n):
-        m, s = divmod(n // 44100, 60)
-        logger.info("Got %02d:%02d of music",m,s)
-    player.set_notify(notify, 10*44100)
-
-    rcpt.pcm = 0
+    response = bottle.response
+    response.content_type = 'audio/x-pcm'
+    status.reset(uri)
     try:
+        status.streaming = True
+        logger.debug('streaming started')
         for data in player.get_data():
-            rcpt.pcm += len(data)
-            status.pos = rcpt.pcm // (2 * 2 * 44100)
+            status.bytes += len(data)
             yield data
-        rcpt.streamed = True
+        status.streamed = True
     except spotutil.PlayError as e:
+        logger.debug('Play error received', e)
         logger.error(str(e))
-        rcpt.failed = True
-        rcpt.error = str(e)
+        status.error = str(e)
     except Exception as e:
+        logger.debug('Exception received', e)
         logger.error(str(e))
-        rcpt.failed = True
-        rcpt.error = str(e)
+        status.error = str(e)
         raise
     finally:
         logger.debug('streaming stopped')
         player.stop() # stop it regardless
-        rcpt.end = time.time()
         status.streaming = False
 
-def flac_encode(input_data, rcpt, *, blksize=8192):
-    try:
-        logger.debug('going to encode via flac')
-        flac = subprocess.Popen([
-            'flac', '--silent', '--force',
-            '--stdout',
-            '--force-raw-format', '--endian=little',
-            '--channels=2', '--bps=16', '--sample-rate=44100', '--sign=signed',
-            '-' ], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-        rcpt.flac = 0
-        feeder = threading.Thread(target=feed_child, daemon=True,
-                    args=(input_data, flac.stdin))
-        feeder.start()
-        while True:
-            data = flac.stdout.read(blksize)
-            if not data: break
-            rcpt.flac += len(data)
-            yield data
-        flac.stdout.close()
-        feeder.join()
-        flac.wait()
-        logger.debug('After flac encode, rcpt=%r', rcpt)
-    except Exception as e:
-        logger.error(str(e))
-        raise
-
-
-def feed_child(source, child):
-    try:
-        for data in source:
-            child.write(data)
-    except Exception as e:
-        logger.err(str(e))
-        raise
-    finally:
-        child.close()
-        logger.debug('finished feeding')
+@app.route('/status')
+def send_status():
+    return status.to_dict()
 
 #
-# Spotify metadata
+# formatting
 #
 
-def get_album_details(uri, artist=True,
-        tracks=True, track_artists=True):
-
-    alb = spotutil.session.get_album(uri)
-    br = alb.browse()
+def format_album(a):
+    br = a.browse()
     br.load()
-    alb.load()
-    logger.debug('loaded')
-    a = Bunch()
-    a.uri = uri
-    a.name = alb.name
-    # include artist as link or object?
-    if not artist:
-        a.artist = str(alb.artist.link)
-    else:
-        artist = a.artist = Bunch()
-        artist.uri = str(alb.artist.link)
-        artist.name = alb.artist.load().name
-    a.year = alb.year
-    a.tracks = []
+    a.load()
+    a.artist.load()
+    tracks = []
+    data = dict(uri=str(a.link), name=a.name, year=a.year,
+                artist=dict(uri=str(a.artist.link), name=a.artist.name),
+                tracks=tracks)
     for t in br.tracks:
-        # just include track links
-        if not tracks:
-            a.tracks.append(str(t.link))
-        else:
-            trk = Bunch()
-            a.tracks.append(trk)
-            t.load()
-            trk.uri = str(t.link)
-            trk.name = t.name
-            trk.duration = t.duration
-            trk.disc = t.disc
-            trk.number = t.index
-            artists = trk.artists = []
-            for ta in t.artists:
-                if not track_artists:
-                    artists.append(str(ta.link))
-                else:
-                    artist = Bunch()
-                    artists.append(artist)
-                    ta.load()
-                    artist.uri = str(ta.link)
-                    artist.name = ta.name
-    return a
+        tracks.append(format_track(t))
+    return data
 
-def get_album_cover(uri):
-    alb = spotutil.session.get_album(uri)
-    if not alb.is_loaded:
-        br = alb.browse()
-        br.load()
-        alb.load()
-        logger.debug('loaded')
-    if not alb.is_loaded:
-        logger.debug('still not loaded')
-    cover = alb.cover()
-    if cover is None: bottle.abort(404)
-    logger.debug('loading cover')
-    cover.load()
-    logger.debug('loaded cover')
-    logger.debug(cover.error)
-    logger.debug(cover.data)
-    return cover.data
+def format_track(t):
+    t.load()
+    artists = []
+    data = dict(uri=str(t.link), name=t.name, duration=t.duration,
+                disc=t.disc, number=t.index, album=str(t.album.link),
+                artists=artists)
+    for ta in t.artists:
+        ta.load()
+        artists.append(dict(uri=str(ta.link), name=ta.name))
+    return data
 
 #
 # Utilities
 #
 
-
 def _expand_uri(prefix, uri):
     if uri.startswith(prefix): return uri
     return prefix + uri
-
-
-def make_receipt(code):
-    if code in receipts:
-        del receipts[code]
-    now = time.time()
-    # clear out old
-    while True:
-        first_rcpt = next(iter(receipts), None)
-        if not first_rcpt: break
-        if 'end' not in first_rcpt: break
-        if first_rcpt.end > (now - receiptPeriod): break
-        receipts.popitem(last=False)
-    rcpt = Bunch()
-    rcpt.start = now
-    receipts[code] = rcpt
-    return rcpt
-
-#def busy():
-#    # marks status as busy
-#    status.lastActive = time.time()
-#    logger.debug("Marked as not idle")
-#
-#def idle_thread(server, timeout):
-#    status.lastActive = time.time()
-#    logger.debug("Will shutdown after %d seconds idle")
-#    while True:
-#        now = time.time()
-#        time_to_next_check = (status.lastActive + timeout) - now
-#        if time_to_next_check <=0: break
-#        time.sleep(time_to_next_check)
-#    logger.info("Shutting down due to inactivity")
-#    server.shutdown()
-
 
 
 if __name__ == '__main__':
